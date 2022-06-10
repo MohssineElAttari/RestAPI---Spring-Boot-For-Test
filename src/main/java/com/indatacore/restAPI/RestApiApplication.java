332a3b6a92081f7d26785e0b2652dd40a4e1c1ec
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-06-10 11:58:42 +0100

    add rest api controller

--- src/main/java/com/indatacore/restAPI/RestApiApplication.java
+++ src/main/java/com/indatacore/restAPI/RestApiApplication.java
@@ -1,7 +1,9 @@
 package com.indatacore.restAPI;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,5 +13,6 @@ public class RestApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApiApplication.class, args);
     }
+
 
 }
