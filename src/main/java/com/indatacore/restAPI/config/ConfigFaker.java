af39ce02709b702e84c4ce92cec0d83b9bee476a
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-06-10 11:59:32 +0100

    configFaker

--- src/main/java/com/indatacore/restAPI/config/ConfigFaker.java
+++ src/main/java/com/indatacore/restAPI/config/ConfigFaker.java
@@ -1,2 +1,13 @@
-package com.indatacore.restAPI.config;public class ConfigFaker {
+package com.indatacore.restAPI.config;
+
+import com.github.javafaker.Faker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigFaker {
+        @Bean
+        public Faker faker() {
+                return new Faker();
+        }
 }
